d61f06b3d8125f04a6e5d9e1455fe1b7e5fab2d5
basti_000 <basti_000@10.18.1.166>
2015-11-27 10:17:30 +0100

    test

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(E_ALL);
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 $host = $_SERVER['HTTP_HOST'];
@@ -13,7 +13,7 @@ $app = new \Slim\Slim(
     'mode' => 'development',
     'debug' => true,
     'view' => new \Slim\Views\Twig(),
-    'templates.path' => '../templates/sites'
+    'templates.path' => '../templates/'
         ]
 );
 
@@ -32,19 +32,19 @@ $twig = $app->view()->getEnvironment();
 $twig->addGlobal('url', 'http://' . $app->request->getHost() . $app->request->getPath());
 
 $app->get('/', function () use ($app) {
-    $app->render('login.html.twig');
+    $app->render('sites/login.html.twig');
 });
 $app->get('/admin', function() use ($app) {
-    $app->render('admin.html.twig');
+    $app->render('sites/admin.html.twig');
 });
 $app->get('/booking/overview', function() use ($app) {
-    $app->render('booking-overview.html.twig');
+    $app->render('sites/booking-overview.html.twig');
 });
 $app->get('/booking/start', function() use ($app) {
-    $app->render('booking_start.html.twig');
+    $app->render('sites/booking_start.html.twig');
 });
 $app->get('/new/event', function() use ($app) {
-    $app->render('new-event.html.twig');
+    $app->render('sites/new-event.html.twig');
 });
 $app->run();
 
